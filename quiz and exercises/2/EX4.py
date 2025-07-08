f1587e31a733909b67994135437ccb12b6ff20e9
Your Name <mahccca@gmail.com>
2025-07-08 15:37:35 +0330

    season 1 and 2 completed

---
+++ quiz and exercises/2/EX4.py
@@ -0,0 +1,9 @@
+import math
+
+coordinates = (4, 3)
+
+origin = (0, 0)
+
+distance = math.sqrt((coordinates[0] - origin[0])**2 + (coordinates[1] - origin[1])**2)
+
+print("Distance from origin to point A:", distance)
---
+++ quiz and exercises/2/Ex1.py
@@ -0,0 +1,4 @@
+text = "This Is A Sample String"
+lower_text = text.lower()           
+words_list = lower_text.split()    
+print(words_list)
